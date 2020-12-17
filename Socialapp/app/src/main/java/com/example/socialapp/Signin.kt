47d887bc72404d23eee4d53d581b518a2df0bfc2
lakshaygarg128 <lakshaygarg128@gmail.com>
2020-12-17 23:41:36 +0530

    SIGN IN FILES
    FILES ADDED

---
+++ Socialapp/app/src/main/java/com/example/socialapp/Signin.kt
@@ -0,0 +1,116 @@
+package com.example.socialapp
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
+class Signin : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private val RC_SIGN_IN: Int = 123
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        auth = Firebase.auth
+        val signinbutton: SignInButton = findViewById(R.id.signinbutton)
+        signinbutton.setOnClickListener {
+            signIn()
+        }
+
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handlesigninresult(task)
+        }
+    }
+
+    private fun handlesigninresult(task: Task<GoogleSignInAccount>?) {
+        try {
+            // Google Sign In was successful, authenticate with Firebase
+            val account = task!!.getResult(ApiException::class.java)!!
+            // Log.d( "firebaseAuthWithGoogle:" + account.id)
+            firebaseAuthWithGoogle(account.idToken!!)
+           // Toast.makeText(this,"Google sign in success", Toast.LENGTH_LONG).show()
+        } catch (e: ApiException) {
+            // Google Sign In failed, update UI appropriately
+            Log.w("Google sign in failed", e)
+            Toast.makeText(this,"Google sign in failed ${e.message}", Toast.LENGTH_LONG).show()
+            // ...
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        GlobalScope.launch(Dispatchers.IO) {
+            var auth = auth.signInWithCredential(credential).await()
+            val firebaseUser = auth.user
+            val progressBar: ProgressBar = findViewById(R.id.progressbar)
+            progressBar.visibility = View.VISIBLE
+            val signinbutton : SignInButton = findViewById((R.id.signinbutton))
+            signinbutton.visibility = View.GONE
+            withContext(Dispatchers.Main)
+            {
+                UpdateUI(firebaseUser)
+            }
+        }
+
+
+    }
+
+
+    private fun UpdateUI(firebaseUser: FirebaseUser?) {
+        if(firebaseUser!=null)
+        {
+            val mainactivityintent = Intent(this,MainActivity::class.java)
+            startActivity(mainactivityintent)
+            finish()
+        }else{
+            val progressBar: ProgressBar = findViewById(R.id.progressbar)
+            progressBar.visibility = View.GONE
+            val signinbutton : SignInButton = findViewById((R.id.signinbutton))
+            signinbutton.visibility = View.VISIBLE
+        }
+    }
+
+}
