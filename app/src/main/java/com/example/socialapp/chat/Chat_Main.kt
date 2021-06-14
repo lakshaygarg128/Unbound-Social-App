31431f5aa3a21c8fc786a0381742e7bdd1f39649
lakshaygarg128 <lakshaygarg128@gmail.com>
2021-06-15 00:52:21 +0530

    chat Button
    Added chat button to the action bar in the top right most corner

---
+++ app/src/main/java/com/example/socialapp/chat/Chat_Main.kt
@@ -0,0 +1,12 @@
+package com.example.socialapp.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.socialapp.R
+
+class Chat_Main : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat__main)
+    }
+}
